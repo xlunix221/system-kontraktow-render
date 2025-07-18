acb4b670c4d7bf048639fa29ce99e8dd59bb3c16
xlunix221 <wiktor.gorzelski@gmail.com>
2025-07-18 23:55:04 +0200

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('./db');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+// ===================================================================================
+//
+//       TWOJE CENTRUM ZARZĄDZANIA - EDYTUJ WSZYSTKO PONIŻEJ
+//
+// ===================================================================================
+
 const initialConfig = {
   maintenanceMode: false,
   users: [
